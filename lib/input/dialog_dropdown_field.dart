65819f9c28b9614f97fbdbba0debf7c19b617361
Daniel <daniel.hoogen@web.de>
2022-12-06 20:56:21 +0100

    dialog dropdown error text direction

--- lib/input/dialog_dropdown_field.dart
+++ lib/input/dialog_dropdown_field.dart
@@ -52,24 +52,21 @@ class _DialogDropdownFieldState<T> extends State<DialogDropdownField<T>> {
                 Expanded(flex: 1, child: Text(widget.label)),
                 SizedBox(
                   width: widget.width + 24,
-                  child: Directionality(
-                    textDirection: TextDirection.rtl,
-                    child: DropdownButtonFormField<T>(
-                      key: _globalKey,
-                      decoration: InputDecoration.collapsed(
-                        hintText: widget.hint,
-                      ),
-                      value: widget.value,
-                      items: widget.items,
-                      onChanged: widget.editable ? widget.onChanged : null,
-                      validator: (value) =>
-                          widget.validator?.validate(context, value),
-                      icon: Icon(
-                        Icons.arrow_drop_down,
-                        color: widget.editable
-                            ? Colors.black
-                            : Theme.of(context).disabledColor,
-                      ),
+                  child: DropdownButtonFormField<T>(
+                    key: _globalKey,
+                    decoration: InputDecoration.collapsed(
+                      hintText: widget.hint,
+                    ),
+                    value: widget.value,
+                    items: widget.items,
+                    onChanged: widget.editable ? widget.onChanged : null,
+                    validator: (value) =>
+                        widget.validator?.validate(context, value),
+                    icon: Icon(
+                      Icons.arrow_drop_down,
+                      color: widget.editable
+                          ? Colors.black
+                          : Theme.of(context).disabledColor,
                     ),
                   ),
                 ),
